deab6513703d5ecf80fb01f6f02885a315d4cc56
Nadiar AS <codxse@gmail.com>
2017-05-13 18:15:58 +0700

    AlbumDetail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -8,12 +9,18 @@ class AlbumList extends Component {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then((response) => response.json())
       .then((responseJson) => this.setState({ albums: responseJson }));
+      // .catch((error) => console.log(error));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {
     return (
       <View>
-        <Text>Album List!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
